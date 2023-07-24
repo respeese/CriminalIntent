c8e2514c9bef1c9bb28ff300259c8fe9cd26376d
ryanspeese <ryanspeese@gmail.com>
2023-07-24 15:56:45 -0400

    Initial commit: added a fragment to MainActivity to show the crime details. Dummy data for now.

---
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.criminalintent
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.example.criminalintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        if(currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+}
