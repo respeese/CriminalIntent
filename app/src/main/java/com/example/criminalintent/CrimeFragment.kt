b66c51ca71abcc3a4bd835da7fd8f78613932894
ryanspeese <ryanspeese@gmail.com>
2023-09-20 13:28:15 -0400

    Attached crimeId as an argument to the CrimeFragment so MainActivity can pass the selected crime's id along to be loaded from DB

--- app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -3,6 +3,7 @@ package com.example.criminalintent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,7 +11,10 @@ import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import java.util.UUID
 
+private const val TAG = "CrimeFragment"
+private const val ARG_CRIME_ID = "crime_id"
 class CrimeFragment : Fragment() {
 
     private lateinit var crime: Crime
@@ -21,6 +25,8 @@ class CrimeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+        val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
+        Log.d(TAG, "args bundle crime ID: $crimeId")
     }
 
     override fun onCreateView(
@@ -63,6 +69,17 @@ class CrimeFragment : Fragment() {
         solvedCheckBox.apply {
             setOnCheckedChangeListener { _, isChecked ->
                 crime.isSolved = isChecked
+            }
+        }
+    }
+
+    companion object {
+        fun newInstance(crimeId: UUID): CrimeFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_CRIME_ID, crimeId)
+            }
+            return CrimeFragment().apply {
+                arguments = args
             }
         }
     }
--- app/src/main/java/com/example/criminalintent/MainActivity.kt
+++ app/src/main/java/com/example/criminalintent/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity(), CrimeListFragment.Callbacks {
     }
 
     override fun onCrimeSelected(crimeId: UUID) {
-        val fragment = CrimeFragment()
+        val fragment = CrimeFragment.newInstance(crimeId)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.fragment_container, fragment)
